a61329e6d0e0dc9b7f04184dc85302b4faa9be92
sivareddy01 <sivanarayanakummitha@gmail.com>
2023-11-07 20:38:16 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def siva(request):
+    return HttpResponse('<h1><marquee>kcpd  cheysi mingutha rey pilla bacha  appasamyam kirikiri<h1></marquee>')
+
+
+def kmpd(request):
+    return HttpResponse('<h1>kernal thread come to my room kadhilo veysi na gadha thoo gibb gibb gibb ani vestha</h1>')
